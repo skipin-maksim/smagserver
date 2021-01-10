3d0f508953538d7c83e092e003e889d0bcff489b
Maksim Skipin <skipysmaks@gmail.com>
2021-01-10 13:59:17 +0200

    remove a all data in server

--- service/ordersServices.js
+++ service/ordersServices.js
@@ -1,7 +1,7 @@
-const moment = require("moment");
-const momentTimezone = require("moment-timezone");
+// const moment = require("moment");
+// const momentTimezone = require("moment-timezone");
 
-const dateNow = momentTimezone().tz("Europe/Kiev").format("DD-MM-YYYY HH:mm");
+// const dateNow = momentTimezone().tz("Europe/Kiev").format("DD-MM-YYYY HH:mm");
 
 const Order = require("./model/orderModel");
 
@@ -14,15 +14,11 @@ const getOrderById = (numOrder) => {
 };
 
 const createOrder = (fields) => {
-  return Order.create({ ...fields, date: dateNow });
+  return Order.create({ ...fields });
 };
 
 const updateOrder = (id, fields) => {
-  return Order.findByIdAndUpdate(
-    { _id: id },
-    { ...fields, dateUpdate: dateNow },
-    { new: true }
-  );
+  return Order.findByIdAndUpdate({ _id: id }, fields, { new: true });
 };
 
 const removeOrder = (id) => {
